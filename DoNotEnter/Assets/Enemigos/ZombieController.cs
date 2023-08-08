df1d246a7589599f1306304cf09e74d2cc69604f
vicsuch <vicentesuch31@gmail.com>
2023-08-08 10:16:17 -0300

    feat: head bob player

--- DoNotEnter/Assets/Enemigos/ZombieController.cs
+++ DoNotEnter/Assets/Enemigos/ZombieController.cs
@@ -4,6 +4,11 @@ using UnityEngine.AI;
 [RequireComponent(typeof(NavMeshAgent))]
 public class ZombieController : MonoBehaviour
 {
+    //Para que la velocidad de los zombies no sean todas iguales
+    //Esto los diferenciara mas y dara mas individualidad
+    [SerializeField] float velVarietion = 0.7f; 
+    [SerializeField] float angularVelVarietion = 10f; 
+    [SerializeField] float accelerationVarietion = 2f; 
     public Transform target;
     Vector3 destination;
     NavMeshAgent agent;
@@ -12,13 +17,16 @@ public class ZombieController : MonoBehaviour
     {
         // Cache agent component and destination
         agent = GetComponent<NavMeshAgent>();
+        agent.speed += Random.Range(-velVarietion, velVarietion);
+        agent.angularSpeed += Random.Range(-angularVelVarietion, angularVelVarietion);
+        agent.acceleration += Random.Range(-accelerationVarietion, accelerationVarietion);
         destination = agent.destination;
     }
 
     void Update()
     {
         // Update destination if the target moves one unit
-        if (Vector3.Distance(destination, target.position) > 1.2f)
+        if (Vector3.Distance(destination, target.position) > 1.5f)
         {
             destination = target.position;
             agent.destination = destination;
