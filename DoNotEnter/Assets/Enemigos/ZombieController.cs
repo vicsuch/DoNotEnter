351a8ad2d1451cea18c325347134a2e756f1c729
vicsuch <vicentesuch31@gmail.com>
2023-08-08 09:19:01 -0300

    feat: ia personajes
    a

---
+++ DoNotEnter/Assets/Enemigos/ZombieController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class ZombieController : MonoBehaviour
+{
+    public Transform target;
+    Vector3 destination;
+    NavMeshAgent agent;
+
+    void Start()
+    {
+        // Cache agent component and destination
+        agent = GetComponent<NavMeshAgent>();
+        destination = agent.destination;
+    }
+
+    void Update()
+    {
+        // Update destination if the target moves one unit
+        if (Vector3.Distance(destination, target.position) > 1.2f)
+        {
+            destination = target.position;
+            agent.destination = destination;
+        }
+    }
+}
