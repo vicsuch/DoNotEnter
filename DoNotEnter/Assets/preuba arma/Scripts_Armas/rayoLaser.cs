1e803d6235900993ee193e5d004aab4337622758
Vicente Such <vicentesuch31@gmail.com>
2023-08-15 08:56:50 -0300

    fix: armas,, agarrar objetos, laser

--- DoNotEnter/Assets/preuba arma/Scripts_Armas/rayoLaser.cs
+++ DoNotEnter/Assets/preuba arma/Scripts_Armas/rayoLaser.cs
@@ -1,24 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
 
 public class rayoLaser : MonoBehaviour
 {
-    public int laserDamageMin = 40;
-    public int laserDamageMax = 55;
+    [SerializeField] private float maxDistance;
+    [SerializeField] private int damage;
+    [SerializeField] private int damageVarietion;
+    [SerializeField] private float laserPersistanceInSeconds;
+    [SerializeField] private float coolDownInSeconds;
+
+    private bool IsCoolDownOver = true;
    
- //   private LineRenderer lineRenderer;
+    private LineRenderer lineRenderer;
 
     // Start is called before the first frame update
     void Start()
     {
-       // lineRenderer = GetComponent<LineRenderer>();
+        lineRenderer = GetComponent<LineRenderer>();
+        lineRenderer.enabled = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown("p"))
+        if (CrossPlatformInputManager.GetButtonDown("Disparar") && IsCoolDownOver)
         {
             ShootLaser();
         }
@@ -26,21 +33,31 @@ public class rayoLaser : MonoBehaviour
     void ShootLaser()
     {
         Ray ray = new Ray(transform.position, transform.forward);
-        RaycastHit[] hits = Physics.RaycastAll(ray, Mathf.Infinity  );
-
-     //   lineRenderer.positionCount = hits.Length * 2;
-
+        RaycastHit[] hits = Physics.RaycastAll(ray, Mathf.Infinity);
+        lineRenderer.SetPosition(0, transform.position);
+        for (int i = 1; i < lineRenderer.positionCount; i++)
+        {
+            lineRenderer.SetPosition(i, Vector3.Lerp(transform.position, transform.forward * maxDistance + transform.position, (i + 1) / lineRenderer.positionCount));
+        }
         for (int i = 0; i < hits.Length; i++)
         {
-           // lineRenderer.SetPosition(i * 2, ray.origin);
-           // lineRenderer.SetPosition(i * 2 + 1, hits[i].point);
-
             vidaenemigo vidaZombieScript = hits[i].collider.GetComponent<vidaenemigo>();
             if (vidaZombieScript != null)
             {
-                int damage = Random.Range(laserDamageMin, laserDamageMax);
-                vidaZombieScript.RestarVida(damage);
+                vidaZombieScript.RestarVida(Random.Range(damage - damageVarietion, damage + damageVarietion));
             }
         }
+        lineRenderer.enabled = true;
+        IsCoolDownOver = false;
+        Invoke("SetCoolDownOver", coolDownInSeconds);
+        Invoke("DesaparecerLinea", laserPersistanceInSeconds);
+    }
+    void DesaparecerLinea()
+    {
+        lineRenderer.enabled = false;
+    }
+    void SetCoolDownOver()
+    {
+        IsCoolDownOver = true;
     }
 }
