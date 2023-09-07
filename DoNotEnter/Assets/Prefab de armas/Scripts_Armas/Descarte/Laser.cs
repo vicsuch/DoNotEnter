565a1364b82e75ebde314c77f1525b82aa195bee
Lucasszejn <josephfiter8@gmail.com>
2023-09-07 15:52:44 -0300

    SUPER COMITT
    Ordena todo el proyecto en carpetas, importa los modelos de Mateo, añade script para que se enciedan las fogatas

---
+++ DoNotEnter/Assets/Prefab de armas/Scripts_Armas/Descarte/Laser.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+    public GameObject jugador; // asignar al fpscontroller
+    private SaludJugador saludJugador;
+    public Transform puntoDisparo; //asignar a la camara
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        saludJugador = jugador.GetComponent<SaludJugador>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("o")) // Cambiar a click
+        {
+            DispararRayoLaser();
+           
+        }
+    }
+    void DispararRayoLaser()
+    {
+        //raycast
+        Ray rayo = new Ray(puntoDisparo.position, puntoDisparo.forward);
+        RaycastHit[] hits = Physics.RaycastAll(rayo, Mathf.Infinity);
+
+        foreach (RaycastHit hit in hits)
+        {
+            //accede al script del enemigo
+            //GameObject enemigo = hit.collider.gameObject;
+           // VidaZombie vidaZombie = enemigo.GetComponent<VidaZombie>();
+       //     VidaZombie vidaZombie = hit.collider.gameObject.GetComponent<VidaZombie>();
+          //  if (vidaZombie != null)
+            {
+                // baja vida
+                int puntuacion = Random.Range(40, 70);
+                //vidaZombie.vida_zombie -= 10;
+            }
+            
+        }
+    }
+}
