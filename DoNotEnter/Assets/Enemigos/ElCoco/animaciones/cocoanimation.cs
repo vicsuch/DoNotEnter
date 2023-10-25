05606624285649a6e737b44b1ae03c860b3fd62b
lucassze <141153220+Lucasszejn@users.noreply.github.com>
2023-10-25 14:02:54 -0300

    Lanzallamas
    Acordrse de bajrle la velocidad al coco

--- DoNotEnter/Assets/Enemigos/ElCoco/animaciones/cocoanimation.cs
+++ DoNotEnter/Assets/Enemigos/ElCoco/animaciones/cocoanimation.cs
@@ -22,6 +22,6 @@ public class cocoanimation : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        anim.SetFloat("velocidad", agent.velocity.magnitude);
+        anim.SetFloat("velociti", agent.velocity.magnitude);
     }
 }
