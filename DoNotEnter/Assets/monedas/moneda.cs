250cc3b33a7df62d1a051fd0a4837722ce2afe99
vicsuch <vicentesuch31@gmail.com>
2023-11-05 19:06:25 -0300

    moneda

--- DoNotEnter/Assets/monedas/moneda.cs
+++ DoNotEnter/Assets/monedas/moneda.cs
@@ -5,21 +5,31 @@ using UnityEngine;
 public class moneda : MonoBehaviour
 {
     public SaludJugador componenteEncontrado;
+    float degresPerSecond = 90f;
+    float unitMovement = 0.5f;
+    float movementSpeed = 1f;
+    float initialY;
+
 
     // Start is called before the first frame update
     void Start()
     {
+        initialY = transform.position.y;
         componenteEncontrado = GameObject.Find("FPSController").GetComponent<SaludJugador>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.rotation = Quaternion.Euler(0f, transform.rotation.eulerAngles.y + degresPerSecond * Time.deltaTime, 0f);
+        transform.position = new Vector3 (transform.position.x, initialY + Mathf.Sin(Time.timeSinceLevelLoad * movementSpeed) * unitMovement, transform.position.z);
     }
     private void OnTriggerEnter(Collider other)
     {
-        componenteEncontrado.monedas_recogidas++;
-        Destroy(gameObject);
+        if(other.CompareTag("Player"))
+        {
+            componenteEncontrado.monedas_recogidas++;
+            Destroy(gameObject);
+        }
     }
 }
