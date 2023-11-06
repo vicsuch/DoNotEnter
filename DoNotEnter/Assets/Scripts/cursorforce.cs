1c521ecdc6a25af6d573aece063da228bea8afe5
Lucasszejn <141153220+Lucasszejn@users.noreply.github.com>
2023-11-06 00:54:08 -0300

    mouse fix

---
+++ DoNotEnter/Assets/Scripts/cursorforce.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cursorforce : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Establece el estado del cursor como visible y desbloqueado
+        Cursor.lockState = CursorLockMode.None;
+        // Hacer que el cursor sea visible
+        Cursor.visible = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
